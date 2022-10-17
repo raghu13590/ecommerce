816df9fa11b937b9ca91229e8b2be84d0a1e620a
raghu <R@ghureddy13>
2022-10-17 00:06:34 -0500

    formatting

--- src/main/java/com/store/ecommerce/service/impl/ProductServiceImpl.java
+++ src/main/java/com/store/ecommerce/service/impl/ProductServiceImpl.java
@@ -100,6 +100,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public List<Product> getAllActiveProducts() {
-        return productRepo.findAll().stream().filter(product -> product.isActive().equals(true)).collect(Collectors.toList());
+        return productRepo.findAll().stream().filter(product -> product.isActive().equals(true))
+                .collect(Collectors.toList());
     }
 }
