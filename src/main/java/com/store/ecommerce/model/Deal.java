60f885d94cd4d0556b44f05b261ee56cc82b3610
raghu <R@ghureddy13>
2022-10-27 01:28:09 -0500

    adding deals' persistence

---
+++ src/main/java/com/store/ecommerce/model/Deal.java
@@ -0,0 +1,82 @@
+package com.store.ecommerce.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Deal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long dealId;
+
+    @Column
+    private LocalDate startTime;
+
+    @Column
+    private LocalDate endTime;
+
+    @Column(nullable = false)
+    private DealType dealType;
+
+    @OneToMany(mappedBy = "productDealId")
+    private Set<ProductDeal> productDeals = new HashSet<>();
+
+    @Column(nullable = false)
+    private Boolean active;
+
+    public Long getDealId() {
+        return dealId;
+    }
+
+    public void setDealId(Long dealId) {
+        this.dealId = dealId;
+    }
+
+    public DealType getDealType() {
+        return dealType;
+    }
+
+    public void setDealType(DealType dealType) {
+        this.dealType = dealType;
+    }
+
+    public Set<ProductDeal> getProductDeals() {
+        return productDeals;
+    }
+
+    public void setProductDeals(Set<ProductDeal> productDeals) {
+        this.productDeals = productDeals;
+    }
+
+    public LocalDate getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDate startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDate getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDate endTime) {
+        this.endTime = endTime;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public enum DealType {
+        BUY_TWO_GET_PRODUCT, BUY_THREE_GET_ONE
+    }
+}
---
+++ src/main/java/com/store/ecommerce/model/ProductDeal.java
@@ -0,0 +1,61 @@
+package com.store.ecommerce.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "PRODUCT_DEAL")
+public class ProductDeal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long productDealId;
+
+    @Column(nullable = false)
+    private ProductDealType productDealType;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "deal_id", nullable = false)
+    private Deal deal;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Deal getDeal() {
+        return deal;
+    }
+
+    public void setDeal(Deal deal) {
+        this.deal = deal;
+    }
+
+
+    public Long getProductDealId() {
+        return productDealId;
+    }
+
+    public void setProductDealId(Long productDealId) {
+        this.productDealId = productDealId;
+    }
+
+    public ProductDealType getProductDealType() {
+        return productDealType;
+    }
+
+    public void setProductDealType(ProductDealType productDealType) {
+        this.productDealType = productDealType;
+    }
+
+    public enum ProductDealType {
+        ELIGIBLE_ITEM, FREE_ITEM
+    }
+}
